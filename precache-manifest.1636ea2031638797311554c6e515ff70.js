d44e123499b3b544c3acad0d255068d406ffdfb4
Cscharf91 <cscharf91@gmail.com>
2020-10-22 13:45:02 -0400

    Updates

---
+++ precache-manifest.1636ea2031638797311554c6e515ff70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad50400ccb2360cac118d78f76c5399",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "e579e7424de1594654d0",
+    "url": "/battleship/static/css/main.04235393.chunk.css"
+  },
+  {
+    "revision": "cf452a4edf00a820051b",
+    "url": "/battleship/static/js/2.b89834e0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/battleship/static/js/2.b89834e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e579e7424de1594654d0",
+    "url": "/battleship/static/js/main.3e512fe5.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
